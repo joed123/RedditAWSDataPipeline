cc423bcda0a0dcae10c4126f25e0014c2e95166c
Joe Di Lullo <85251118+joed123@users.noreply.github.com>
2025-01-06 13:03:44 -0800

    Update aws_etl.py

--- etls/aws_etl.py
+++ etls/aws_etl.py
@@ -1,9 +1,11 @@
 import s3fs
+from utils.constants import AWS_ACCESS_KEY_ID, AWS_ACCESS_KEY
 
 def connect_to_s3():
     try:
-        s3 = s3fs.S3FileSystem(
-                            #Removed for security
+        s3 = s3fs.S3FileSystem(anon=False,
+                               key= AWS_ACCESS_KEY_ID,
+                               secret=AWS_ACCESS_KEY
                                )
         return s3
     except Exception as e:
