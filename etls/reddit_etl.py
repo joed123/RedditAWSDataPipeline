0cb1fddd809050f7af7ed6eb14d1c19b4711e352
Joe Di Lullo <85251118+joed123@users.noreply.github.com>
2025-01-06 13:04:38 -0800

    Update reddit_etl.py

--- etls/reddit_etl.py
+++ etls/reddit_etl.py
@@ -9,9 +9,9 @@ from utils.constants import POST_FIELDS
 
 def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
     try:
-        reddit = praw.Reddit(
-                            #Removed for security
-                             )
+        reddit = praw.Reddit(client_id=client_id,
+                             client_secret=client_secret,
+                             user_agent=user_agent)
         print("We are connected to Reddit")
         return reddit
     except Exception as e:
